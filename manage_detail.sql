de3ccab316c8a6725233741a386d219fdd589b12
点点 <102277652+1474416640@users.noreply.github.com>
2023-10-16 23:36:02 +0800

    4day

--- manage_detail.sql
+++ manage_detail.sql
@@ -1,1 +1,10 @@
--- 
+-- 说明 用户管理的细节
+-- 在创建用户的时候，如果不指定Host,则为%,% 表示所有ip都可以连接权限，create user XXX;
+CREATE USER jack;
+SELECT `host`,`user` FROM mysql.user;
+-- 你也可以这样指定 create user 'XXX' @'192.168.1.%' 表示xxx 用户在192.168.1* 的ip可以登录MySQL
+CREATE USER 'tiantian'@'192.168.1.%'
+-- 在删除用户的时候，如果host 不是%,需要明确指定’用户‘@'host值'
+DROP USER jack  -- 默认就是 drop user 'jack'@'%'
+DROP USER tiantian  -- 失败
+DROP USER 'tiantian'@'192.168.1.%'
